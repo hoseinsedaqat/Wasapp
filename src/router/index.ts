62a568552370376936639dfa62996924fe020105
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-06-01 12:46:13 +0330

    add/route-utils

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { redirectRoute } from '@/utils/redirectRoute';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'HomeView',
-      component: async () => await import('@/views/Home/HomeView.vue')
+      beforeEnter:redirectRoute,
+      component: async () => await import('@/views/Home/HomeView.vue'),
     },
     {
       path: '/web/whatsapp',
---
+++ src/utils/redirectRoute.ts
@@ -0,0 +1,4 @@
+// eslint-diable-next-line
+export function redirectRoute(to:any,from:any,next:any){
+    next({path: "/web/whatsapp"})
+}
